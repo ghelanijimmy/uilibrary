c1edc5d996ce6e17bca70f7a8fcc6d62c9d7ff33
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2020-08-31 13:27:50 -0400

    stories and story config

---
+++ .storybook/preview.js
@@ -0,0 +1,8 @@
+import { addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+
+addDecorator(withInfo);
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" }
+}
