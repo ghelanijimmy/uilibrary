ad6d0ca2c865d665e76031a6bf8b6059f1dd10a0
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2020-09-01 08:41:06 -0400

    initial button styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,77 @@
 import React from "react";
 import * as PropTypes from "prop-types";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
+import {button} from "../constants/button";
 
-const ButtonSC = styled.button``;
+const {PRIMARY, SECONDARY} = button;
 
-const Button = ({text=''}) => {
-	return (
-		<ButtonSC>
-			{text}
-		</ButtonSC>
-	);
+const ButtonSC = styled.button`
+	padding: 20px;
+	border-radius: 4px;
+	cursor: pointer;
+	background-color: transparent;
+	border: 1px solid #333;
+	transition: .25s all ease-in-out;
+	
+	&:hover {
+		background-color: #333;
+		border: 1px solid #333;
+		color: #fff;
+	}
+`;
+
+const PrimaryButtonSC = styled(ButtonSC)`
+	${({theme})=>css`
+		color: ${theme.light.button.primary.regular.color};
+		background-color: ${theme.light.button.primary.regular.bgColor};
+		border: 1px solid ${theme.light.button.primary.regular.borderColor};
+		
+		&:hover {
+			background-color: ${theme.light.button.primary.regular.hoverBGColor};
+			color: ${theme.light.button.primary.regular.hoverColor};
+			border-color: ${theme.light.button.primary.regular.hoverBorderColor};
+		}
+	`};
+`;
+
+const SecondaryButtonSC = styled(ButtonSC)`
+	${({theme})=>css`
+		color: ${theme.light.button.secondary.regular.color};
+		background-color: ${theme.light.button.secondary.regular.bgColor};
+		border: 1px solid ${theme.light.button.secondary.regular.borderColor};
+		
+		&:hover {
+			background-color: ${theme.light.button.secondary.regular.hoverBGColor};
+			color: ${theme.light.button.secondary.regular.hoverColor};
+			border-color: ${theme.light.button.secondary.regular.hoverBorderColor};
+		}
+	`};
+`;
+
+const Button = ({text='', type=''}) => {
+	switch (type) {
+		case PRIMARY:
+			return (
+				<PrimaryButtonSC>
+					{text}
+				</PrimaryButtonSC>
+			);
+		case SECONDARY:
+			return (
+				<SecondaryButtonSC>
+					{text}
+				</SecondaryButtonSC>
+			)
+		default:
+			return (
+				<ButtonSC>
+					{text}
+				</ButtonSC>
+			);
+	}
 };
 Button.propTypes = {
-	text: PropTypes.string
+	text: PropTypes.string,
+	type: PropTypes.string
 }
 export default Button;
---
+++ src/constants/button.js
@@ -0,0 +1,4 @@
+export const button = {
+	PRIMARY: 'primary',
+	SECONDARY: 'secondary'
+}
